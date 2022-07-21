c826e6ab215f99564b33288fd6c55ac7cd92cc8e
Hsn07 <hbacak15@posta.pau.edu.tr>
2022-07-21 22:55:32 +0300

    Google Maps
    Seçilen Şubeye ait konum google mapde  gösterildi.

--- app/src/main/java/com/hbacakk/banka/ui/fragmentSubeDetay/SubeDetayFragment.java
+++ app/src/main/java/com/hbacakk/banka/ui/fragmentSubeDetay/SubeDetayFragment.java
@@ -1,16 +1,21 @@
 package com.hbacakk.banka.ui.fragmentSubeDetay;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.hbacakk.banka.R;
 import com.hbacakk.banka.data.models.Sube;
+import com.hbacakk.banka.databinding.DialogInfoBinding;
 import com.hbacakk.banka.databinding.FragmentSubeDetayBinding;
 
 public class SubeDetayFragment extends Fragment {
@@ -40,5 +45,48 @@ public class SubeDetayFragment extends Fragment {
 
         detayBinding.setSube(sube);
 
+        detayBinding.layoutYolTarifiAl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sube.Adres != null && sube.Adres.length() > 10) {
+                    alertDialog("Google Maps Açılıyor", "Konum bilgisi için google maps açılacaktır. Devam etmek istiyor musunuz?", sube.Adres);
+                } else {
+                    Toast.makeText(getActivity(), "Hatalı veya eksik adress", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    private void alertDialog(String title, String message, String adress) {
+        AlertDialog.Builder _builder = new AlertDialog.Builder(getActivity());
+        DialogInfoBinding dialogInfoBinding = DataBindingUtil.inflate(LayoutInflater.from(getActivity()),
+                R.layout.dialog_info, null, false);
+
+        _builder.setView(dialogInfoBinding.getRoot());
+
+        AlertDialog dialog = _builder.create();
+
+        dialogInfoBinding.setTitle(title);
+        dialogInfoBinding.setMessage(message);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+
+        dialogInfoBinding.buttonOk.setOnClickListener(view -> {
+            openTheGoogleMaps(adress);
+            dialog.dismiss();
+        });
+        dialogInfoBinding.imageViewClose.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+
+    private void openTheGoogleMaps(String adress) {
+        String uri = "http://maps.google.co.in/maps?q=" + adress;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(intent);
+    }
+
 }
